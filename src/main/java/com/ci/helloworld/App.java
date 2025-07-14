8cc112d1eb61a9732473cb5bd9efa82094ea3792
suraj061199 <2021wb86745@wilp.bits-pilani.ac.in>
2025-07-14 22:02:15 +0530

    Task 4: Trigger CI by modifying source code

--- src/main/java/com/ci/helloworld/App.java
+++ src/main/java/com/ci/helloworld/App.java
@@ -2,7 +2,7 @@ package com.ci.helloworld;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins CI/CD!");
+        System.out.println("Updated to test CI in Task 4");
     }
 }
 
